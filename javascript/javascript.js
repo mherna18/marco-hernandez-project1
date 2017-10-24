e36622225dde1bb1e750c94efe18338fa4ecbf77
mherna18 <marcohernandez29@gmail.com>
2017-10-24 14:47:17 -0500

    added another javascript function

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -3,3 +3,13 @@ function submit_button()
 	alert("Thank you for signing up!");
 	return true;
 }
+
+function text()
+{
+		var first_name= document.getElementById("first_name");
+	first_name.value = first_name.value.toUpperCase();
+
+	var last_name= document.getElementById("last_name");
+	last_name.value = last_name.value.toUpperCase();
+
+}
